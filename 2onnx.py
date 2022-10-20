81f827fad1e21323a2e06be4abe49fae5dcfcb15
chenpengfei <2578925789@@qq.com>
2022-10-20 09:30:48 +0800

    1、增加训练命令记录；2、增加模型库打印；3、增加转onnx代码；

---
+++ 2onnx.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+"""PyTorch Inference Script
+
+An example inference script that outputs top-k class ids for images in a folder into a csv.
+
+Hacked together by / Copyright 2020 Ross Wightman (https://github.com/rwightman)
+"""
+import os
+import time
+import argparse
+import logging
+import numpy as np
+import torch
+import onnx
+
+from timm.models import create_model, apply_test_time_pool
+from timm.data import ImageDataset, create_loader, resolve_data_config
+from timm.utils import AverageMeter, setup_default_logging
+
+torch.backends.cudnn.benchmark = True
+_logger = logging.getLogger('inference')
+
+
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Inference')
+parser.add_argument('data', metavar='DIR',
+                    help='path to dataset')
+parser.add_argument('--output_dir', metavar='DIR', default='./',
+                    help='path to output files')
+parser.add_argument('--model', '-m', metavar='MODEL', default='dpn92',
+                    help='model architecture (default: dpn92)')
+parser.add_argument('-j', '--workers', default=2, type=int, metavar='N',
+                    help='number of data loading workers (default: 2)')
+parser.add_argument('-b', '--batch-size', default=256, type=int,
+                    metavar='N', help='mini-batch size (default: 256)')
+parser.add_argument('--img-size', default=None, type=int,
+                    metavar='N', help='Input image dimension')
+parser.add_argument('--input-size', default=None, nargs=3, type=int,
+                    metavar='N N N', help='Input all image dimensions (d h w, e.g. --input-size 3 224 224), uses model default if empty')
+parser.add_argument('--mean', type=float, nargs='+', default=None, metavar='MEAN',
+                    help='Override mean pixel value of dataset')
+parser.add_argument('--std', type=float, nargs='+', default=None, metavar='STD',
+                    help='Override std deviation of of dataset')
+parser.add_argument('--interpolation', default='', type=str, metavar='NAME',
+                    help='Image resize interpolation type (overrides model)')
+parser.add_argument('--num-classes', type=int, default=1000,
+                    help='Number classes in dataset')
+parser.add_argument('--log-freq', default=10, type=int,
+                    metavar='N', help='batch logging frequency (default: 10)')
+parser.add_argument('--checkpoint', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('--pretrained', dest='pretrained', action='store_true',
+                    help='use pre-trained model')
+parser.add_argument('--num-gpu', type=int, default=1,
+                    help='Number of GPUS to use')
+parser.add_argument('--no-test-pool', dest='no_test_pool', action='store_true',
+                    help='disable test time pool')
+parser.add_argument('--topk', default=5, type=int,
+                    metavar='N', help='Top-k to output to CSV')
+
+
+def main():
+    setup_default_logging()
+    args = parser.parse_args()
+    # might as well try to do something useful...
+    args.pretrained = args.pretrained or not args.checkpoint
+
+    # create model
+    model = create_model(
+        args.model,
+        num_classes=args.num_classes,
+        in_chans=3,
+        pretrained=args.pretrained,
+        checkpoint_path=args.checkpoint)
+
+    _logger.info('Model %s created, param count: %d' %
+                 (args.model, sum([m.numel() for m in model.parameters()])))
+
+    config = resolve_data_config(vars(args), model=model)
+    model, test_time_pool = (model, False) if args.no_test_pool else apply_test_time_pool(model, config)
+
+    if args.num_gpu > 1:
+        model = torch.nn.DataParallel(model, device_ids=list(range(args.num_gpu))).cuda()
+    else:
+        model = model.cuda()
+
+    model.eval()
+
+    onnx_name = "./output/train/20221019-211135-mobilenetv2_050-128/model_best.onnx"
+    with torch.no_grad():
+        input = torch.randn(1, 3, 128, 128).cuda()
+        labels = model(input)
+        torch.onnx.export(model, input, onnx_name, verbose=False, opset_version=10,
+                          input_names=["input"], output_names=['classifier'])
+        print('onnx export success!')
+        onnx_model = onnx.load(onnx_name)
+        onnx.checker.check_model(onnx_model)  # assuming throw on error
+        print("==> Passed")
+
+
+
+if __name__ == '__main__':
+    main()
